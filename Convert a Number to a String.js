26dd876783f11d8403dace6504a9df1defa9432f
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-11 13:08:41 +0530

    8 kyu

---
+++ Convert a Number to a String.js
@@ -0,0 +1,19 @@
+
+//Convert a number to a string
+
+My solution
+
+function numberToString(num) {
+return ''+num;
+}
+Other solutions
+
+function numberToString(num) {
+return num.toString();
+}
+
+const numberToString = num => `${num}`;
+
+function numberToString2(num) {
+return String(num);
+}
