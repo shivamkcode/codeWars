6f102c74e6208088341f6692c3a6a6cbf823b719
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-18 22:08:16 +0530

    8 kyu

---
+++ UEFA EURO 2016.js
@@ -0,0 +1,16 @@
+/*Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+
+uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+
+uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."*/
+
+function uefaEuro2016(teams, scores){
+   let winner = scores[0] > scores[1] ? teams[0] + " won!" : teams[1] + " won!"  
+   if(scores[0] === scores[1]){
+         winner =  "teams played draw."
+     }
+    return `At match ${teams[0]} - ${teams[1]}, ${winner}`
+    
+}
