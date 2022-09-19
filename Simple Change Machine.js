694d2a56b800a95251b555f041e4cf9b561d4d58
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-19 23:07:46 +0530

    8 kyu

---
+++ Simple Change Machine.js
@@ -0,0 +1,3 @@
+function changeMe(m){
+  return {"£5":"20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p","£2":"20p 20p 20p 20p 20p 20p 20p 20p 20p 20p","£1":"20p 20p 20p 20p 20p","50p":"20p 20p 10p","20p":"10p 10p"}[m]||m
+}
