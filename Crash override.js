9332e733ba697a65f70e6c80bf5ae0c53ec44650
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-19 22:08:20 +0530

    8 kyu

---
+++ Crash override.js
@@ -0,0 +1,40 @@
+/* 
+
+
+
+
+
+Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+
+Your task is to create a function that, given a proper first and last name, will return the correct alias.
+
+Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given.
+
+If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+
+var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+
+aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+
+aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.'
+
+Happy hacking!*/
+function aliasGen(a,b){
+
+    if('0123456789'.includes(a[0]) || '0123456789'.includes(b[0])){
+
+       return 'Your name must start with a letter from A - Z.'
+
+       }
+
+  a = a.toUpperCase()
+
+  b = b.toUpperCase()
+
+  return `${firstName[a[0]]} ${surname[b[0]]}`
+
+}
