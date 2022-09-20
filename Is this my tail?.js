7f895509301e1b5021963764bd5cb9812dca2ef0
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-20 18:17:01 +0530

    8 kyu

---
+++ Is this my tail?.js
@@ -0,0 +1,23 @@
+function correctTail(body, tail){
+
+  
+
+  let sub = body.substr(body.length-tail.length)
+
+                    
+
+  
+
+  if( sub === tail) 
+
+    return true
+
+  
+
+  else 
+
+    return false
+
+  
+
+}
