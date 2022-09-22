21c391c48f833b6f51ffa6839d80b28c7ed41b33
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-23 00:26:17 +0530

    update You're a square.js

--- 7 kyu/You're a square.js
+++ 7 kyu/You're a square.js
@@ -16,5 +16,15 @@ Examples
  4  =>  true
 25  =>  true*/
 
-
+var isSquare = function(n){
+  for (var x = 0; x <= n; x++) {
+    if (n === 0) {
+      return true;
+    } else if (n / x === x) {
+      return true;
+    }
+  }
+  
+  return false;
+}
 
