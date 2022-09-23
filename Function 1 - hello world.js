8c89ecce8e49baf0800e54b04db13f22a2e3cbeb
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-23 08:36:14 +0530

    update Function 1 - hello world.js

--- Function 1 - hello world.js
+++ Function 1 - hello world.js
@@ -7,3 +7,29 @@ Sure, this is about as easy as it gets. But how clever can you be to create the 
 // Write a function "greet" that returns "hello world!"
 
 const greet = str => "hello world!"
+
+//Most creative solution I found
+
+function greet(){
+  var a=0
+    var abc=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var hello = [];
+    var world = [];
+                    a++;    a++;  a++;a++;  a++;      a++;      a++;a--;a++;
+hello.push(abc[a]); a++;    a++;  a--;      a--;      a--;      a--;    a--;
+hello.push(abc[a]); a++;a++;a++;  a++;a--;  a++;      a++;      a++;    a++;
+hello.push(abc[a]); a++;    a++;  a--;      a--;      a++;      a--;    a=a;
+hello.push(abc[a]); a++;    a--;  a++;a++;  a--;a++;  a--;a++;  a++;a--;a++;
+hello.push(abc[a]);
+
+           
+                    a++;          a++;  a++;a++;a++;  a++;a--;a++;  a=a;      a++;a++;
+world.push(abc[a]);  a--;        a--;   a--;    a--;  a--;    a--;  a--;      a--;   a=a;
+world.push(abc[a]);   a++;      a++;    a++;    a--;  a--;a--;      a++;      a++;    a++;
+world.push(abc[a]);    a--;a++;a--;     a--;    a--;  a--;  a++;    a--;      a--;   a--;
+world.push(abc[a]);     a--;  a--;      a--;a--;a--;  a--;    a--;  a--;a++;  a=a;a--;
+world.push(abc[a]);
+
+  return hello.join("") + " " + world.join("")+"!";
+}
