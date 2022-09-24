1e8a770005cf02c484730eaf1c1a14e1c7462d36
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-24 18:48:16 +0530

    update A needle in the haystack.js

--- A needle in the haystack.js
+++ A needle in the haystack.js
@@ -16,3 +16,6 @@ function findNeedle(haystack) {
     return `found the needle at position ${pos}`
 }
 
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle");
+}
