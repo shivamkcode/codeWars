4a72a068509a1d821f4a7df1427b9e25efc3f36c
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-25 23:51:44 +0530

    update Get the middle character of string.js

--- 7 kyu/Get the middle character of string.js
+++ 7 kyu/Get the middle character of string.js
@@ -1,7 +1,6 @@
 //You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
 function getMiddle(str) {
-
     var position;
     var length;
     if (str.length % 2 == 1) {
