f930d4e36efe6d45fb06a049a038b3dbd1f21cef
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-25 23:51:37 +0530

    update Get the middle character of string.js

--- 7 kyu/Get the middle character of string.js
+++ 7 kyu/Get the middle character of string.js
@@ -3,23 +3,13 @@
 function getMiddle(str) {
 
     var position;
-
     var length;
-
     if (str.length % 2 == 1) {
-
         position = str.length / 2;
-
         length = 1;
-
     } else {
-
         position = str.length / 2 - 1;
-
         length = 2;
-
     }
-
     return str.substring(position, position + length)
-
 }
