f64eb80f05bfbdc4bc7ee13e5232a0232d65ed18
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-26 07:09:14 +0530

    update colorGhost.js

---
+++ colorGhost.js
@@ -0,0 +1,17 @@
+/*Create a class Ghost
+
+Ghost objects are instantiated without any arguments.
+
+Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+ghost = new Ghost();
+ghost.color //=> "white" or "yellow" or "purple" or "red"*/
+
+class Ghost {
+  constructor(){
+    this.colors = ["white", "yellow", "purple", "red"]
+  }
+  get color(){
+    return this.colors[Math.floor(Math.random() * this.colors.length)]
+  }
+};
