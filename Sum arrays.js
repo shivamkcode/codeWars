89bca1023911c89ad0cf547e3b52e99c8f961faa
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-27 06:27:58 +0530

    update Sum arrays.js

---
+++ Sum arrays.js
@@ -0,0 +1,11 @@
+//Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+function sum (numbers) {
+    "use strict";
+  let sum = 0
+    if(numbers.length < 1){
+      return sum
+      }
+    numbers.forEach(x => sum += x)
+  return sum
+};
