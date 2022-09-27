e8c8ac8a79877183691972d4e1a5c45b94cba9c5
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-27 06:36:12 +0530

    update Calculate average.js

--- Calculate average.js
+++ Calculate average.js
@@ -2,3 +2,9 @@
 
 //Note: Empty arrays should return 0.
 
+function findAverage(array) {
+  // your code here
+ let sum = 0
+ array.forEach(x => sum += x)
+  return array.length>0 ? sum/array.length : sum
+}
