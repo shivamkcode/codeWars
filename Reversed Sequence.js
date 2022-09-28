bf15fa5afc252af7e6b7f0df44ec16c2e4ff42c7
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-28 06:15:01 +0530

    update Reversed Sequence.js

--- Reversed Sequence.js
+++ Reversed Sequence.js
@@ -19,3 +19,10 @@ let arr = [];
     } return arr;
 };
 
+const reverseSeq = n => {
+  return Array(n).fill(0).map((e, i) => n - i );
+};
+
+/*Array(n) creates an array with n number of empty spots. Ex: Array(5) gives you this: [ undefined, undefined, undefined, undefined, undefined ].
+Then comes fill(0), the fill() method changes all elements in an array to a static value(MDN Web docs). Ex: Our array from the example above, after applying .fill(0) would be [0, 0, 0, 0, 0].
+Finally .map takes each element of the array and runs a provided function on each element. Ex: [0, 0, 0, 0, 0].map( ( e, i ) => n-i ); equals to [5,4,3,2,1].*/
