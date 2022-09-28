72057c562156f5caef3dad06bf09336074eb4231
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-28 22:53:12 +0530

    update List Filtering.js

--- 7 kyu/List Filtering.js
+++ 7 kyu/List Filtering.js
@@ -5,3 +5,8 @@ filter_list([1,2,'a','b']) == [1,2]
 filter_list([1,'a','b',0,15]) == [1,0,15]
 filter_list([1,2,'aasf','1','123',123]) == [1,2,123]*/
 
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  return l.filter(x => x !== String(x) )
+}
+
