314a3220d2e2dfdafa99af201777dde475a84b2a
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-29 23:09:56 +0530

    update Calculate BMI.js

--- Calculate BMI.js
+++ Calculate BMI.js
@@ -8,3 +8,16 @@ if bmi <= 30.0 return "Overweight"
 
 if bmi > 30 return "Obese"*/
 
+function bmi(weight, height) {
+  let bm = weight/(height**2)
+  if(bm <= 18.5){
+    return "Underweight"
+    }else if(bm <= 25){
+    return "Normal"
+    }else if(bm <= 30){
+    return "Overweight"
+    }else{
+      return "Obese"
+      }
+}
+
