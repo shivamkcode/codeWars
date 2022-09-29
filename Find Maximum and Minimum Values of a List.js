b356ab78840a64a17ee97db1f065b5d8114f7c62
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-29 23:17:44 +0530

    update Find Maximum and Minimum Values of a List.js

--- Find Maximum and Minimum Values of a List.js
+++ Find Maximum and Minimum Values of a List.js
@@ -15,3 +15,15 @@ var max = function(list){
     
     return Math.max(...list);
 }
+
+//Using sort method
+
+var min = function(list){
+    list.sort((a, b) => (a - b));
+    return list[0];
+}
+
+var max = function(list){
+    list.sort((a, b) => (b - a));
+    return list[0];
+}
