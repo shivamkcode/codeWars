42c619d677118d9804624cd30bfa01bad7c63dc2
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-09-30 22:13:13 +0530

    update Count By X.js

--- Count By X.js
+++ Count By X.js
@@ -8,3 +8,12 @@ Examples
 countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 countBy(2,5) === [2,4,6,8,10]*/
 
+function countBy(x, n) {
+  let z = [];
+  for(let i = 1; i<= x*n; i++){
+    if(i%x === 0){
+       z.push(i)
+      }
+    }
+  return z;
+}
