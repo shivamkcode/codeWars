8bffb037ae1ddd7e75967ad5c625d4434893ab54
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-02 00:11:41 +0530

    update Shortest word.js

---
+++ 7 kyu/Shortest word.js
@@ -0,0 +1,18 @@
+/*Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.*/
+
+function findShort(s) {
+    let arr = s.split(' ')
+    let l = arr[0].length
+
+    for (let i = 0; i < arr.length; i++) {
+        if (l > arr[i].length) {
+            l = arr[i].length
+        } else {
+            l
+        }
+    }
+    return l
+}
+
