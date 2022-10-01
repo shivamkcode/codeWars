3512e179a6a2e121fce275c5727723b903a3d7fb
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-01 23:57:29 +0530

    update Jaden casing Strings.js

--- 7 kyu/Jaden casing Strings.js
+++ 7 kyu/Jaden casing Strings.js
@@ -7,3 +7,8 @@ Example:
 Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"*/
 
+String.prototype.toJadenCase = function() {
+    return this.split(" ").map((word) => {
+        return word[0].toUpperCase() + word.slice(1);
+    }).join(" ");
+};
