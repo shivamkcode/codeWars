453903318facd4b902012da73c42433d706ca9af
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-01 06:20:54 +0530

    update Rock Paper Scissors.js

--- Rock Paper Scissors.js
+++ Rock Paper Scissors.js
@@ -7,3 +7,7 @@ Examples(Input1, Input2 --> Output):
 "scissors", "rock" --> "Player 2 won!"
 "paper", "paper" --> "Draw!"*/
 
+const rps = (p1, p2) => {
+  return p1 === p2 ? 'Draw!' : 
+  p1 === 'rock' && p2 === 'scissors' || p1 === 'paper' && p2 === 'rock' || p1 === 'scissors' && p2 === 'paper' ? 'Player 1 won!' : 'Player 2 won!'                 
+};
