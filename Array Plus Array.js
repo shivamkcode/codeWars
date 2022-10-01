75a701b7a17b79e18c935d28d9a8a8dc93494c0a
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-01 22:53:52 +0530

    update Array Plus Array.js

--- Array Plus Array.js
+++ Array Plus Array.js
@@ -6,3 +6,6 @@ function arrayPlusArray(arr1, arr2) {
   return arr1.reduce((a,b)=> a+b) + arr2.reduce((a,b) => a+b);
 }
 
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((a, b) => a + b)
+}
