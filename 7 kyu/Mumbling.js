178c3200929923be91ee2c2172ffc89553862e2f
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-03 15:45:20 +0530

    update Mumbling.js

--- 7 kyu/Mumbling.js
+++ 7 kyu/Mumbling.js
@@ -6,3 +6,10 @@ accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt") -> "C-Ww-Aaa-Tttt"
 The parameter of accum is a string which includes only letters from a..z and A..Z.*/
 
+function accum(s) {
+
+    let str = s.toLowerCase().split('')
+    let arr = str.map((x, i) => x.repeat(i+1))
+    return arr.map(e => e.charAt(0).toUpperCase() + e.substr(1)).join('-')
+}
+
