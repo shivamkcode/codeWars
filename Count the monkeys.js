5a4cebc2e6e12efa7252bc52df4be0818fe4f8c0
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-04 14:59:26 +0530

    update Count the monkeys.js

--- Count the monkeys.js
+++ Count the monkeys.js
@@ -6,5 +6,13 @@ For example(Input --> Output):
 
 10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
  1 --> [1]*/
- 
- 
+
+
+function monkeyCount(n) {
+    let arr = []
+    for (i = 1; i <= n; i++) {
+        arr.push(i)
+    }
+    return arr
+}
+
