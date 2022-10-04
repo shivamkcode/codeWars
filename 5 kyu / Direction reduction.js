d1794fcf8376be1c89e10f48d8f3c4602a996730
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-04 18:26:06 +0530

    update  Direction reduction.js

--- 5 kyu / Direction reduction.js
+++ 5 kyu / Direction reduction.js
@@ -23,3 +23,18 @@ function dirReduc(arr){
     })
     return result
 }
+
+function dirReduc(arr){
+  var count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "WEST" && arr[i+1] === "EAST" ||
+        arr[i] === "EAST" && arr[i+1] === "WEST" ||
+        arr[i] === "NORTH" && arr[i+1] === "SOUTH" ||
+        arr[i] === "SOUTH" && arr[i+1] === "NORTH") {
+        arr.splice(i, 2);
+        count++;
+        i--;
+    }
+  }
+  return count === 0 ? arr : dirReduc(arr);
+}
