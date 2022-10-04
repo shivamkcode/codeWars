77208ab890231d1cee0657acf2a511ddfb1c75fc
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-04 16:23:27 +0530

    update Sum of two lowest positive integers.js

--- 7 kyu/Sum of two lowest positive integers.js
+++ 7 kyu/Sum of two lowest positive integers.js
@@ -4,3 +4,7 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 
 [10, 343445353, 3453445, 3453545353453] should return 3453455.*/
 
+function sumTwoSmallestNumbers(numbers) {  
+  let n = numbers.sort((a,b) => a - b)
+  return n[0] + n[1]
+}
