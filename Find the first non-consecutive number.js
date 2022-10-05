0eed759315eb7b7d206afd23dc90b2ce2c18d481
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-05 15:16:30 +0530

    update Find the first non-consecutive number.js

--- Find the first non-consecutive number.js
+++ Find the first non-consecutive number.js
@@ -15,3 +15,11 @@ function firstNonConsecutive (arr) {
         }
     }return null
 }
+
+//or
+
+function firstNonConsecutive (arr) {
+    let result = arr.find((val, index) => val !== index + arr[0]);
+
+    return (Number.isInteger(result)) ? result : null;
+}
