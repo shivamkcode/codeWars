1ceb7d073cb0fe39b4e64a8e3e0b3c732bf3f8f1
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-05 16:34:58 +0530

    update Twice as old.js

--- Twice as old.js
+++ Twice as old.js
@@ -4,3 +4,12 @@ current father's age (years)
 current age of his son (years)
 Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.*/
 
+function twiceAsOld(dad, son) {
+
+    for (let i = 0; i <= dad; dad/2 > son ? i++: i--) {
+        if (son + i == (dad + i)/2) {
+            return i >= 0 ? i: -i
+        }
+    }
+}
+
