e52dc16f52f8c944fa1239f70224a1166f5faa8c
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-05 16:50:28 +0530

    update Switch it Up!.js

--- Switch it Up!.js
+++ Switch it Up!.js
@@ -6,3 +6,15 @@ Output :: "One".
 
 If your language supports it, try using a switch statement.*/
 
+function switchItUp(n){
+//Write your own Code!
+  return n == 1 ? 'One' :
+  n == 2 ? 'Two' :
+  n == 3 ? 'Three' :
+  n == 4 ? 'Four' :
+  n == 5 ? 'Five' :
+  n == 6 ? 'Six' :
+  n == 7 ? 'Seven' :
+  n == 8 ? 'Eight' : 
+  n == 9 ? 'Nine' : 'Zero'
+}
