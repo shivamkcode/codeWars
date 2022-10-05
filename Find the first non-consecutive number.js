e7d3df3e8812228dd79b5fc2d7963faa3aa989b0
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-05 15:09:36 +0530

    update Find the first non-consecutive number.js

--- Find the first non-consecutive number.js
+++ Find the first non-consecutive number.js
@@ -8,3 +8,10 @@ If the whole array is consecutive then return null2.
 
 The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!*/
 
+function firstNonConsecutive (arr) {
+    for (let i = 1; i < arr.length; i++) {
+        if ((arr[i - 1] + 1) !== arr[i]) {
+            return arr[i]
+        }
+    }return null
+}
