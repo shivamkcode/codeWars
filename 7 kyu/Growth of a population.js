c3855b0048913b42282d3cd758c0ab89e5ac1c89
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-06 23:25:00 +0530

    update Growth of a population.js

--- 7 kyu/Growth of a population.js
+++ 7 kyu/Growth of a population.js
@@ -33,3 +33,9 @@ function nbYear(p0, percent, aug, p) {
   return years
 }
 
+function nbYear(p0, percent, aug, p, years = 0) {
+  if (p0 < p) {
+    return nbYear(p0 + Math.floor(p0 * percent / 100) + aug, percent, aug, p, ++years);
+  }
+  return years;
+}
