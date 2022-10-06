30f630efa687b3762b1913c060f4daa337bc57cc
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-06 06:32:48 +0530

    update Will there be enough space?.js

--- Will there be enough space?.js
+++ Will there be enough space?.js
@@ -10,3 +10,8 @@ on is the number of people on the bus excluding the driver.
 wait is the number of people waiting to get on to the bus excluding the driver.
 If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.*/
 
+
+function enough(cap, on, wait) {
+  let num = cap - on - wait
+  return num >= 0 ? 0 : -num
+}
