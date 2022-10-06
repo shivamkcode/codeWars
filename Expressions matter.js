34a295aedcc97dadf5b3f166d7cef5f0a9e72dae
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-06 20:40:48 +0530

    update Expressions matter.js

---
+++ Expressions matter.js
@@ -0,0 +1,5 @@
+/*Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained (Read the notes for more detail about it)
+Example
+With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:*/
+
