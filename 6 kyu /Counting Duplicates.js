eaefb4b62c3d3ffb620f76a527e13a2d0b586065
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-07 22:39:37 +0530

    update Counting Duplicates.js

--- 6 kyu /Counting Duplicates.js
+++ 6 kyu /Counting Duplicates.js
@@ -1,7 +1,3 @@
- kyu
-Counting Duplicates
-331165390% of 16,66948,968 of 159,023kgashok
- JavaScript
 /*Count the number of Duplicates
 Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
 
