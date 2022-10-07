b2b618e3ea47e74755c181bfe599c31a664804d0
shivamkcode <108574110+shivamkcode@users.noreply.github.com>
2022-10-07 06:19:39 +0530

    update Grasshopper - Messi Goals.js

--- Grasshopper - Messi Goals.js
+++ Grasshopper - Messi Goals.js
@@ -15,3 +15,8 @@ championsLeagueGoals
 copaDelReyGoals
 Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.*/
 
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
