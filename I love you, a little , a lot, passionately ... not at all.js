458d30149efbb9c66aa88f983892c9d947f85477
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-09 16:17:51 +0530

    update I love you, a little , a lot, passionately ... not at all.js

--- I love you, a little , a lot, passionately ... not at all.js
+++ I love you, a little , a lot, passionately ... not at all.js
@@ -22,3 +22,16 @@ function howMuchILoveYou(n) {
     num === 5 ? 'madly' :
     'not at all'
 }
+
+const phrases = [
+    'I love you',
+    'a little',
+    'a lot',
+    'passionately',
+    'madly',
+    'not at all',
+]
+
+function howMuchILoveYou(n) {
+     return phrases[(n - 1) % phrases.length] 
+}
