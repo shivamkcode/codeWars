e1c92e7d3ae2a36a08e2c0b5bf91147ad136ce4f
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-11 00:03:06 +0530

    update Is it a pallindrom?.js

--- Is it a pallindrom?.js
+++ Is it a pallindrom?.js
@@ -4,3 +4,6 @@ function isPalindrome(x) {
   return x.toLowerCase() == x.toLowerCase().split('').reverse().join('')
 }
 
+function isPalindrome(x, str = x.toLowerCase()) {
+  return str === [...str].reverse().join('')
+}
