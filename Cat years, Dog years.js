b61876cae07cc14139851013f09b3aa59f92a3e3
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-10 10:58:31 +0530

    update Cat years, Dog years.js

--- Cat years, Dog years.js
+++ Cat years, Dog years.js
@@ -1,4 +1,28 @@
+/*Kata Task
+I have a cat and a dog.
+
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+
+humanYears >= 1
+humanYears are whole numbers only
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that*/
+
 var humanYearsCatYearsDogYears = function(humanYears) {
-  let cat = humanYears === 1 ? 15 :
-  humanYears === 2 ? 24 : 24 + (humanYears - 2) * 4
-  let dog = humanYears === 1 ? 15 :
+    let cat = humanYears === 1 ? 15:
+    humanYears === 2 ? 24: 24 + (humanYears - 2) * 4
+    let dog = humanYears === 1 ? 15:
+    humanYears === 2 ? 24: 24 + (humanYears - 2) * 5
+    return [humanYears…cat,
+        dog]
+}
