e91ddbc69558c16e2eb4a45345144081508100a2
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-11 22:47:59 +0530

    update Replace With Alphabet Position.js

--- 6 kyu /Replace With Alphabet Position.js
+++ 6 kyu /Replace With Alphabet Position.js
@@ -19,3 +19,14 @@ function alphabetPosition(tex) {
     }
     return num.join(' ')
 }
+
+//Other way
+function alphabetPosition(text) {
+  var result = "";
+  for (var i = 0; i < text.length; i++){
+    var code = text.toUpperCase().charCodeAt(i)
+    if (code > 64 && code < 91) result += (code - 64) + " ";
+  }
+
+  return result.slice(0, result.length-1);
+}
