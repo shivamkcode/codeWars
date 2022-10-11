25af200e71afbac84c84226aecf88e618a60b18b
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-11 16:14:40 +0530

    update Filter out the geese.js

--- Filter out the geese.js
+++ Filter out the geese.js
@@ -20,3 +20,12 @@ Your function would return the following array:
 ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
 The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.*/
 
+function gooseFilter (birds) {
+    var geese = ["African",
+        "Roman Tufted",
+        "Toulouse",
+        "Pilgrim",
+        "Steinbacher"];
+    return birds.filter(x => !geese.includes(x))
+    // return an array containing all of the strings in the input array except those that match strings in geese
+};
