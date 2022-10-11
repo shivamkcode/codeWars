1eed6b6c8b073d6392619da57043a8ed552044e9
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-11 22:45:49 +0530

    update Replace With Alphabet Position.js

--- 6 kyu /Replace With Alphabet Position.js
+++ 6 kyu /Replace With Alphabet Position.js
@@ -8,3 +8,14 @@ Example
 alphabetPosition("The sunset sets at twelve o' clock.")
 Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )*/
 
+function alphabetPosition(tex) {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    let num = []
+    let text = tex.toLowerCase().split(' ').join('')
+    for (let i = 0; i < text.length; i++) {
+        if (alphabet.includes(text[i])) {
+            num.push(alphabet.indexOf(text[i]) + 1)
+        }
+    }
+    return num.join(' ')
+}
