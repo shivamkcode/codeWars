a5dd8fd01db4ad2d94c134c7814119f7403b149d
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-12 23:51:23 +0530

    update Regex validate pin.js

--- 7 kyu/Regex validate pin.js
+++ 7 kyu/Regex validate pin.js
@@ -16,3 +16,12 @@ function validatePIN (pin) {
     }
 }
 
+function validatePIN (pin) {
+    pin = pin.split('')
+    const findNaN = pin.find(character => !(parseInt(character) >= 0))
+    if ((pin.length === 4 || pin.length === 6) && !findNaN) {
+        return true
+    } else {
+        return false
+    }
+}
