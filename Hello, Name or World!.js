7ad7277f40fb3f319d97819e45ad24c83da71690
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-14 20:56:59 +0530

    update Hello, Name or World!.js

--- Hello, Name or World!.js
+++ Hello, Name or World!.js
@@ -13,5 +13,22 @@ function hello(n) {
     return n ? `Hello, ${n.slice(0, 1).toUpperCase()}${n.slice(1).toLowerCase()}!`: 'Hello, World!'
 }
 
+
+
 const hello = s =>
 `Hello, ${s ? (s[0].toUpperCase() + s.slice(1).toLowerCase()): 'World'}!`;
+
+
+
+Object.defineProperty(String.prototype, 'capitalize', {
+    enumerable: false,
+    configurable: false,
+    writeable: false,
+    value: function() {
+        return this.slice(0, 1).toUpperCase() + this.slice(1).toLowerCase();
+    }
+});
+
+function hello(name) {
+    return 'Hello, ' + (!name?'world': name).capitalize() + '!';
+}
