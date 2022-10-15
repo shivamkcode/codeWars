6a05cf1cb93201dc101ff4a8dd6904f052692f10
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-15 23:16:06 +0530

    update 101 Dalmatians - squash the bugs, not the dogs!.js

--- 101 Dalmatians - squash the bugs, not the dogs!.js
+++ 101 Dalmatians - squash the bugs, not the dogs!.js
@@ -16,3 +16,15 @@ function howManyDalmatians (number) {
     return number <= 10 ? dogs[0]: number <= 50 ? dogs[1]: number === 101 ? dogs[3]: dogs[2];
 
 }
+
+function howManyDalmatians(number) {
+    if (number <= 10) {
+        return "Hardly any"
+    } else if (number <= 50) {
+        return "More than a handful!"
+    } else if (number === 101) {
+        return "101 DALMATIANS!!!"
+    } else {
+        return "Woah that's a lot of dogs!"
+    }
+}
