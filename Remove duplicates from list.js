aff9cf2091678d53bde8d6ab39938e327e0cdca5
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-15 09:06:13 +0530

    update Remove duplicates from list.js

--- Remove duplicates from list.js
+++ Remove duplicates from list.js
@@ -9,3 +9,13 @@ function distinct(a) {
 const distinct = a => a.filter((item, index) => a.indexOf(item) === index);
 
 
+function distinct(arr) {
+  let res = []; 
+  
+  for(let i = 0; i < arr.length; i++){
+    if(!res.includes(arr[i])){
+      res.push(arr[i]);
+    }
+}
+return res;
+}
