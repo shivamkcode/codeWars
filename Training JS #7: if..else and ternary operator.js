ec9af66df0d0710ccb7f80451086199f8e0acd12
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-15 23:27:32 +0530

    update Training JS #7: if..else and ternary operator.js

--- Training JS #7: if..else and ternary operator.js
+++ Training JS #7: if..else and ternary operator.js
@@ -11,3 +11,10 @@ function saleHotdogs(n){
   n < 10 ? n*95 : n*90
 }
 
+const saleHotdogs = (n) => {
+  switch (true) {
+    case n < 5: return n * 100;
+    case n < 10: return n * 95;
+    default: return n * 90;
+  }
+}
