8afbdf17f82744bd419519e5be301dc0b72b6e9b
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-16 14:02:28 +0530

    update Exclamation marks series #1: Remove an exclamation mark from the end of string.js

--- Exclamation marks series #1: Remove an exclamation mark from the end of string.js
+++ Exclamation marks series #1: Remove an exclamation mark from the end of string.js
@@ -9,9 +9,12 @@ remove("Hi! Hi!") == "Hi! Hi"
 remove("Hi") == "Hi"*/
 
 function remove (string) {
-  let s = string.split('')
-  s[s.length-1] === '!' ? s.pop() : s
-  return s.join('')
+    let s = string.split('')
+    s[s.length-1] === '!' ? s.pop(): s
+    return s.join('')
 }
 
 
+function remove(s) {
+    return s.endsWith('!') ? s.slice(0, -1): s;
+}
