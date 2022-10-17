b8c06f1671b6a74f1b2e815e02c9dcf2bc93a1d8
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-17 22:30:08 +0530

    update Simple validation of a username with regex.js

--- Simple validation of a username with regex.js
+++ Simple validation of a username with regex.js
@@ -10,3 +10,7 @@ Length should be between 4 and 16 characters (both included).
 const validateUsr = (username) => {
     return username.length >= 4 && username.length <= 16 && (username.match(/[a-z]|_|\d/g).length === username.length);
 };
+
+function validateUsr(username) {
+  return /^[0-9a-z_]{4,16}$/.test(username)
+}
