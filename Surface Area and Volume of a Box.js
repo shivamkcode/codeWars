7f748abd39243fc4ec0ea83db004cd86e0944d03
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-17 07:37:52 +0530

    update Surface Area and Volume of a Box.js

--- Surface Area and Volume of a Box.js
+++ Surface Area and Volume of a Box.js
@@ -5,3 +5,19 @@ function getSize(w, h, d) {
         w*h*d]
 }
 
+
+
+function getSize(width, height, depth){
+  var box = { width: width, height:height, depth:depth};
+  
+  return [getArea(box), getVolume(box)];
+}
+
+
+function getArea(box){
+  return (2 * (box.depth * box.height)) + (2 * (box.width * box.height)) + (2 * (box.width * box.depth));
+}
+
+function getVolume(box){
+  return box.width * box.height * box.depth;
+}
