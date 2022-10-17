f8f1e9e275bb8cbba1df0c84ef8abe7d0b6e5a3a
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-17 22:27:18 +0530

    update Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence.js

--- Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence.js
+++ Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence.js
@@ -6,8 +6,28 @@ replace("!Hi! Hi!") === "!H!! H!!"
 replace("aeiou") === "!!!!!"
 replace("ABCDE") === "!BCD!"*/
 
-function replace(s){
-  //coding and coding....
-  let vowels = 'AEIOUaeiou'
-  return s.split('').map(x => vowels.includes(x) ? '!' : x).join('')
+function replace(s) {
+    //coding and coding....
+    let vowels = 'AEIOUaeiou'
+    return s.split('').map(x => vowels.includes(x) ? '!': x).join('')
+}
+
+function replace(s) {
+  let sArr = s.split('');
+  
+  for (let i = 0; i < s.length; i += 1) {
+    if (s[i] === 'a' ||
+        s[i] === 'e' ||
+        s[i] === 'i' ||
+        s[i] === 'o' ||
+        s[i] === 'u' ||
+        s[i] === 'A' ||
+        s[i] === 'E' ||
+        s[i] === 'I' ||
+        s[i] === 'O' ||
+        s[i] === 'U') {
+      sArr[i] = '!';
+    }
+  }
+  return sArr.join('');
 }
