10b4116c041da1766fafb8d0a4521345b8020502
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-17 07:36:41 +0530

    update Surface Area and Volume of a Box.js

--- Surface Area and Volume of a Box.js
+++ Surface Area and Volume of a Box.js
@@ -1,2 +1,7 @@
 /*Write a function that returns the total surface area and volume of a box as an array: [area, volume]*/
 
+function getSize(w, h, d) {
+    return [2*w*h + 2*w*d + 2*h*d,
+        w*h*d]
+}
+
