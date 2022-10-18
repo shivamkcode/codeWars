6340a3411e8d1c1bb89c804113de315bf1285e0c
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-18 07:58:59 +0530

    update Sleigh Authentication.js

--- Sleigh Authentication.js
+++ Sleigh Authentication.js
@@ -11,3 +11,9 @@ sleigh.authenticate("Santa", "Ho Ho Ho!"); // must return FALSE
 sleigh.authenticate("Santa Claus", "Ho Ho!"); // must return FALSE
 sleigh.authenticate("jhoffner", "CodeWars"); // Nope, even Jake is not allowed to use the sleigh ;)*/
 
+function Sleigh() {}
+
+Sleigh.prototype.authenticate = function(name, password) {
+  return name === 'Santa Claus' && password === 'Ho Ho Ho!'
+};
+
