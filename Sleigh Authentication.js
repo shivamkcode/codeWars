3fa56bf85fe5ba24396ee273e79fed120beb8697
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-18 08:00:55 +0530

    update Sleigh Authentication.js

--- Sleigh Authentication.js
+++ Sleigh Authentication.js
@@ -17,3 +17,11 @@ Sleigh.prototype.authenticate = function(name, password) {
   return name === 'Santa Claus' && password === 'Ho Ho Ho!'
 };
 
+function Sleigh() {
+  this.name = "Santa Claus";
+  this.password = "Ho Ho Ho!";
+}
+
+Sleigh.prototype.authenticate = function(name, password) {
+  return this.name == name && this.password == password;
+};
