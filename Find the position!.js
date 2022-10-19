c9c315cc0784cd84f15a027867ee55228279848a
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-19 23:11:40 +0530

    update Find the position!.js

--- Find the position!.js
+++ Find the position!.js
@@ -4,3 +4,8 @@ Input :: "a"
 
 Ouput :: "Position of alphabet: 1"*/
 
+function position(letter) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz"
+    let pos = alphabet.indexOf(letter) + 1
+    return `Position of alphabet: ${pos}`
+}
