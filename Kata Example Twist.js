2526f094c0665177ae9102e104cd6fb011244952
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-19 15:15:36 +0530

    update Kata Example Twist.js

--- Kata Example Twist.js
+++ Kata Example Twist.js
@@ -2,3 +2,5 @@
 
 Add the value "codewars" to the array websites/Websites 1,000 times.*/
 
+//add the value "codewars" to the websites array 1,000 times
+var websites = new Array(1000).fill('codewars')
