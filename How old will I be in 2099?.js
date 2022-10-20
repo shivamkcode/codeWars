2beed45c7d0db0042821f64701893c9ac7cf5499
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-20 08:42:02 +0530

    update How old will I be in 2099?.js

--- How old will I be in 2099?.js
+++ How old will I be in 2099?.js
@@ -14,3 +14,12 @@ function calculateAge(dob, year) {
     year === dob ? `You were born this very year!`: `You will be born in ${dob - year} ${years}.`
 }
 
+let calculateAge = (old, news) => {
+  if ( old === news ) {
+    return "You were born this very year!" 
+  }else if ( news > old ) {
+    return news - old === 1 ? "You are 1 year old." : `You are ${news - old} years old.`
+  }else{
+    return old - news === 1 ? "You will be born in 1 year." : `You will be born in ${old - news} years.`
+  } 
+}
