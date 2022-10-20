11bdf09f5f35c2fdbae54fc4620ad5a7d7867f2e
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-20 17:13:38 +0530

    update Sum of differences in array.js

--- Sum of differences in array.js
+++ Sum of differences in array.js
@@ -8,3 +8,6 @@ Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
 
 If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).*/
 
+function sumOfDifferences(arr) {
+    return arr.length > 1 ? Math.max(...arr) - Math.min(...arr): 0
+}
