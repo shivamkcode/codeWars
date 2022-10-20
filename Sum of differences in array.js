9918cd44c8d44975e546733f8722fe654174f8cb
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-20 17:18:22 +0530

    update Sum of differences in array.js

--- Sum of differences in array.js
+++ Sum of differences in array.js
@@ -16,3 +16,16 @@ const sumOfDifferences = arr => arr
 .sort((a, b) => b - a)
 .map((a, i) => a - arr[i + 1] || 0)
 .reduce((a, b) => a + b, 0);
+
+
+function sumOfDifferences(arr) {
+    let sorted = arr.sort((a, b) => b - a)
+    let sum = 0;
+    for (let i = 0; i < sorted.length - 1; i++) {
+        sum += (arr[i] - arr[i + 1]);
+    }
+    return sum;
+}
+
+const sumOfDifferences = arr =>
+arr.sort((a, b) => b - a).shift() - arr.pop() || 0;
