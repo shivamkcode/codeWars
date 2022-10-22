ef0b4bf9a4a5c7ae1306228e2b390626dbe50437
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-22 23:04:31 +0530

    update Basic subclasses - Adam and Eve.js

--- Basic subclasses - Adam and Eve.js
+++ Basic subclasses - Adam and Eve.js
@@ -26,3 +26,16 @@ class Woman extends Human {
         super(name);
     }
 }
+
+class God {
+    /**
+    * @returns Human[]
+    */
+    static create() {
+        return [new Man,
+            new Woman];
+    }
+}
+class Human {}
+class Man extends Human {}
+class Woman extends Human {}
