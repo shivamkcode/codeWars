232f9236a1546cd4620b18026289277d109bf661
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-22 22:58:57 +0530

    update Pillars.js

--- Pillars.js
+++ Pillars.js
@@ -9,3 +9,10 @@ function pillars(num, dist, width) {
     return num === 1 ? 0: (num - 1) * dist * 100 + (num - 2) * width
 }
 
+function pillars(num_pill, dist, width) {
+    if (num_pill < 2) return 0;
+    const widthAllPillars = (num_pill - 2) * width;
+    const distAll = (num_pill - 1) * (dist * 100);
+
+    return distAll + widthAllPillars;
+}
