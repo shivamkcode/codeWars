ede4ccb00f954e0e9ad479cde81728d72c5ecc61
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-23 22:53:27 +0530

    update Regexp Basics - is it a digit?.js

--- Regexp Basics - is it a digit?.js
+++ Regexp Basics - is it a digit?.js
@@ -1,2 +1,7 @@
 //Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
 
+String.prototype.digit = function() {
+
+    let digit = '0123456789'.split('')
+    return digit.includes(this.toString())
+};
