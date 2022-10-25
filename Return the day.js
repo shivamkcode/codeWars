ba8803b94291ce0c74416e2dde27da19dbd7e726
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-25 22:57:20 +0530

    update Return the day.js

--- Return the day.js
+++ Return the day.js
@@ -9,3 +9,12 @@
 7 returns "Saturday"
 Otherwise returns "Wrong, please enter a number between 1 and 7"*/
 
+function whatday(n) {
+    return n === 1 ? 'Sunday':
+    n === 2 ? 'Monday':
+    n === 3 ? 'Tuesday':
+    n === 4 ? 'Wednesday':
+    n === 5 ? 'Thursday':
+    n === 6 ? 'Friday':
+    n === 7 ? 'Saturday': "Wrong, please enter a number between 1 and 7"
+}
