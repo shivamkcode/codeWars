112eaae983713c9b52d69a3e6a3a08a133e4f6e8
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-25 22:58:10 +0530

    update Return the day.js

--- Return the day.js
+++ Return the day.js
@@ -18,3 +18,9 @@ function whatday(n) {
     n === 6 ? 'Friday':
     n === 7 ? 'Saturday': "Wrong, please enter a number between 1 and 7"
 }
+
+function whatday(num) { 
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return days[num - 1] || 'Wrong, please enter a number between 1 and 7';
+}
+
