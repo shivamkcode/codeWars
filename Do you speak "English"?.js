3c8d3918a9bce19d5e52c9759d981c6d46da8c4f
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-25 23:03:32 +0530

    update Do you speak "English"?.js

--- Do you speak "English"?.js
+++ Do you speak "English"?.js
@@ -6,3 +6,7 @@ Upper or lower case letter does not matter -- "eNglisH" is also correct.
 
 Return value as boolean values, true for the string to contains "English", false for it does not.*/
 
+function spEng(sentence) {
+    let en = 'english'
+    return sentence.toLowerCase().includes(en)
+}
