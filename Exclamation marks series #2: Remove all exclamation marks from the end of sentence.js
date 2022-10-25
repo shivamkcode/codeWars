bda79fe65ee685d107c563a3565671e01c289bce
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-25 22:39:50 +0530

    update Exclamation marks series #2: Remove all exclamation marks from the end of sentence.js

--- Exclamation marks series #2: Remove all exclamation marks from the end of sentence.js
+++ Exclamation marks series #2: Remove all exclamation marks from the end of sentence.js
@@ -8,3 +8,10 @@ remove("!Hi!") === "!Hi"
 remove("Hi! Hi!") === "Hi! Hi"
 remove("Hi") === "Hi"*/
 
+function remove(s) {
+    while (s && s.slice(-1) == '!') {
+        s = s.slice(0, -1);
+    }
+    return s;
+}
+
