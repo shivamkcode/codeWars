61a49cd569df12bfe0984a03ba448e6aff7fa976
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-26 22:34:36 +0530

    update Fundamentals: Return.js

--- Fundamentals: Return.js
+++ Fundamentals: Return.js
@@ -16,3 +16,26 @@ subtraction = subt
 
 Note: All math operations will be: a (operation) b*/
 
+function add(a, b) {
+    return a + b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function mod(a, b) {
+    return a % b
+}
+
+function exponent(a, b) {
+    return a**b
+}
+
+function subt(a, b) {
+    return a - b
+}
