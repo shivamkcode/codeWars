1dddfb992882c3cac980d7363a934875a4474eb8
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-26 22:35:46 +0530

    update Fundamentals: Return.js

--- Fundamentals: Return.js
+++ Fundamentals: Return.js
@@ -39,3 +39,10 @@ function exponent(a, b) {
 function subt(a, b) {
     return a - b
 }
+
+const add = (a, b) => a + b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+const mod = (a, b) => a % b;
+const exponent = (a, b) => a ** b;
+const subt = (a, b) => a - b;
