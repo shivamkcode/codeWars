3c640c34f5b56d94a9633d3d810a00a5702f8caf
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-26 23:23:13 +0530

    update Training JS #8: Conditional statement--switch.js

--- Training JS #8: Conditional statement--switch.js
+++ Training JS #8: Conditional statement--switch.js
@@ -89,3 +89,14 @@ function howManydays(month) {
         }
         return days;
     }
+
+    function howManydays(month) {
+        switch (month) {
+            case 2: return 28
+                case 4:
+                    case 6:
+                        case 9:
+                            case 11: return 30
+                        }
+                        return 31
+                }
