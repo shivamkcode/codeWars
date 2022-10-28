f671495a3a84c406abe2fa3261bb2aa9c0ac1f5e
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-28 23:33:28 +0530

    update Tip calculator.js

--- Tip calculator.js
+++ Tip calculator.js
@@ -19,3 +19,17 @@ function calculateTip(amount, rating) {
     rating === 'great' ? Math.ceil(amount * 0.15):
     rating === 'excellent' ? Math.ceil(amount * 0.2): 'Rating not recognised'
 }
+
+const TIPS = {
+    "terrible": 0.0,
+    "poor": 0.05,
+    "good": 0.1,
+    "great": 0.15,
+    "excellent": 0.2
+};
+
+const calculateTip = (amount, rating) => {
+    rating = rating.toLowerCase();
+
+    return rating in TIPS ? Math.ceil(TIPS[rating] * amount): "Rating not recognised";
+};
