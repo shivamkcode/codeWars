6ece9f3e6b0e3cd7fa8d895dbb6f9ecb7117bf53
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-28 23:38:11 +0530

    update Type of sum.js

--- Type of sum.js
+++ Type of sum.js
@@ -1,2 +1,7 @@
 //Return the type of the sum of the two arguments
 
+const typeOfSum = (a, b) => typeof(a + b);
+
+function typeOfSum(a, b) {
+    return typeof (a+b) === 'number' ? 'number': 'string';
+}
