70087ddc0d93ef4932642199cf3824237d49060c
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-28 23:31:34 +0530

    update Tip calculator.js

--- Tip calculator.js
+++ Tip calculator.js
@@ -11,3 +11,11 @@ The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating
 
 "Rating not recognised" in*/
 
+function calculateTip(amount, rating) {
+    rating = rating.toLowerCase()
+    return rating === 'terrible' ? 0:
+    rating === 'poor' ? Math.ceil(amount * 0.05):
+    rating === 'good' ? Math.ceil(amount * 0.1):
+    rating === 'great' ? Math.ceil(amount * 0.15):
+    rating === 'excellent' ? Math.ceil(amount * 0.2): 'Rating not recognised'
+}
