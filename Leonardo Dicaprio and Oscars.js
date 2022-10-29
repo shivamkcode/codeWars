a085708cac0dc381789bca33e9ee633e1eef55db
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-29 06:39:02 +0530

    update Leonardo Dicaprio and Oscars.js

--- Leonardo Dicaprio and Oscars.js
+++ Leonardo Dicaprio and Oscars.js
@@ -8,3 +8,8 @@ if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
 if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
 if it was over 88 you should return "Leo got one already!"*/
 
+function leo(oscar) {
+    return oscar > 88 ? "Leo got one already!":
+    oscar === 88 ? "Leo finally won the oscar! Leo is happy":
+    oscar === 86 ? "Not even for Wolf of wallstreet?!": "When will you give Leo an Oscar?"
+}
