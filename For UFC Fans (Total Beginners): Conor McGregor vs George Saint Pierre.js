12f8ac23f333b20de435d975a55eee173d3e4cf9
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-31 23:15:07 +0530

    update For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.js

--- For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.js
+++ For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.js
@@ -12,3 +12,8 @@ var quote = function(fighter) {
     return fighter.toLowerCase() === 'george saint pierre' ? "I am not impressed by your performance.": "I'd like to take this chance to apologize.. To absolutely NOBODY!"
 };
 
+function quote(fighter) {
+  switch (fighter.toLowerCase()) {
+    case 'george saint pierre': return 'I am not impressed by your performance.';
+    case 'conor mcgregor': return 'I\'d like to take this chance to apologize.. To absolutely NOBODY!';
+  }
