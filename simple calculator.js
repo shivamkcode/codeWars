f80c5e1ac97b16a6de8f2afc1442270c1bf01c95
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-10-31 23:22:38 +0530

    update simple calculator.js

--- simple calculator.js
+++ simple calculator.js
@@ -17,3 +17,19 @@ function calculator(a, b, sign) {
         sign === '-' ? a - b:
         sign === '/' ? a / b: 'unknown value'): 'unknown value'
 }
+
+function calculator(a, b, sign) {
+    if ((typeof a === "number") && (typeof b === "number")) {
+        switch (sign) {
+            case "+":
+                return a + b;
+                case "-":
+                    return a - b;
+                    case "*":
+                        return a * b;
+                        case "/":
+                            return a / b;
+                    }
+            }
+            return "unknown value";
+        }
