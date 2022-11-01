f538492a845610b7e2005e1e845f3706c1a55699
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-11-01 22:52:59 +0530

    update Fix your code before the garden dies!.js

--- Fix your code before the garden dies!.js
+++ Fix your code before the garden dies!.js
@@ -9,3 +9,7 @@ function rainAmount(mm) {
         return "Your plant has had more than enough water for today!"
     }
 }
+
+function rainAmount(mm) {
+    return mm < 40 ? `You need to give your plant ${40 - mm}mm of water`: "Your plant has had more than enough water for today!"
+}
