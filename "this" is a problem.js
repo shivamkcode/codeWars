10fa024f6b4249d81dc2c5561a558b1d477a0a8e
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-11-03 23:22:23 +0530

    update "this" is a problem.js

--- "this" is a problem.js
+++ "this" is a problem.js
@@ -12,3 +12,10 @@ var n = new NameMe('John', 'Doe');
 n.firstName //Expected: John
 n.lastName //Expected: Doe
 n.name //Expected: John Doe*/
+
+function NameMe(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    this.name = first + ' ' + last
+}
+
