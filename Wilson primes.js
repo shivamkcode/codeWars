4e52922a607b18349d0a486b589c20e7c7bd7330
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-11-05 09:03:07 +0530

    update Wilson primes.js

--- Wilson primes.js
+++ Wilson primes.js
@@ -11,3 +11,10 @@ function amIWilson(p) {
     // "The only known Wilson primes are 5, 13 and 563."
     return p == 5 || p == 13 || p == 563
 }
+
+function amIWilson(p) {
+    p = BigInt(p);
+    const fac = n => n ? n * fac(n-1n): 1n;
+    const d = (fac(p-1n) + 1n);
+    return !(d % (p * p));
+}
