b996e3d3e00df8ebb2a879d97fdbbb3406674c2c
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-11-05 09:02:46 +0530

    update Wilson primes.js

--- Wilson primes.js
+++ Wilson primes.js
@@ -7,3 +7,7 @@ should give a whole number.
 
 Your task is to create a function that returns true if the given number is a Wilson prime.*/
 
+function amIWilson(p) {
+    // "The only known Wilson primes are 5, 13 and 563."
+    return p == 5 || p == 13 || p == 563
+}
