695d4fb74c44dd2b9eabfbdb7f483626943d0828
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-11-06 22:41:08 +0530

    update Geometry Basics: Distance between points in 2D.js

--- Geometry Basics: Distance between points in 2D.js
+++ Geometry Basics: Distance between points in 2D.js
@@ -10,3 +10,6 @@ const distanceBetweenPoints = (a, b) => {
     return Math.sqrt(Math.abs(a.x - b.x) ** 2 + Math.abs(a.y - b.y) ** 2);
 };
 
+function distanceBetweenPoints(a, b) {
+  return Math.hypot(a.x - b.x, a.y - b.y);
+}
