4ee00221b87e139a23b9742740eb9cc72d96fa52
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-11-06 23:02:28 +0530

    update Duck duck goose.js

--- Duck duck goose.js
+++ Duck duck goose.js
@@ -11,3 +11,6 @@ duck_duck_goose([$a, $b, $c, $d], 1); // => $a["name"]
 duck_duck_goose([$a, $b, $c, $d], 5); // => $a["name"]
 duck_duck_goose([$a, $b, $c, $d], 4); // => $d["name"]*/
 
+function duckDuckGoose(players, goose) {
+    return players[(goose - 1) % players.length].name
+}
