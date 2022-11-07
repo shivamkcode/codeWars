74149b5f1fb7a89d647d57ff5ad7a8d497a1b98f
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-11-07 07:37:13 +0530

    update Barking mad.js

--- Barking mad.js
+++ Barking mad.js
@@ -3,3 +3,32 @@
 snoopy.bark(); // return "Woof"
 scoobydoo.bark(); // undefined
 Use method prototypes to enable all Dogs to bark.*/
+
+
+function Dog (breed) {
+    this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function() {
+    return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
+scoobydoo.bark = function() {
+    return "Woof";
+};
+
+
+function Dog (breed) {
+    this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+Dog.prototype.bark = function() {
+    return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
