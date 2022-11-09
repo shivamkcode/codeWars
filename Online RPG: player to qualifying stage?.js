13961a182c09e4b35e679c57d6d6942f0e4f1890
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-11-09 23:24:28 +0530

    update Online RPG: player to qualifying stage?.js

--- Online RPG: player to qualifying stage?.js
+++ Online RPG: player to qualifying stage?.js
@@ -11,3 +11,10 @@ Otherwise return, False/false*/
 function playerRankUp (points) {
     return points >= 100 ? 'Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.': false
 }
+
+function playerRankUp (points) {
+    if (points < 100) {
+        return false;
+    }
+    return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.";
+}
