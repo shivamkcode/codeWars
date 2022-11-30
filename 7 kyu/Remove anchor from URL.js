bebe9a87f617c49fbd265b038322bab9ee27cc23
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-11-30 21:33:10 +0530

    update Remove anchor from URL.js

--- 7 kyu/Remove anchor from URL.js
+++ 7 kyu/Remove anchor from URL.js
@@ -4,3 +4,6 @@ Examples
 "www.codewars.com#about" --> "www.codewars.com"
 "www.codewars.com?page=1" -->"www.codewars.com?page=1"*/
 
+function removeUrlAnchor(url) {
+    return url.slice(0, url.indexOf('#') !== -1? url.indexOf('#'): url.length)
+}
