52658c9493af6e56f57618f86d636f2a743d4977
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-12-02 21:50:35 +0530

    update The highest profit wins!.js

--- 7 kyu/The highest profit wins!.js
+++ 7 kyu/The highest profit wins!.js
@@ -12,3 +12,11 @@ function minMax(arr) {
     return [Math.min(...arr),
         Math.max(...arr)]; // fix me!
 }
+
+function minMax(arr) {
+    var a = arr.sort(function(a, b) {
+        return a-b
+    });
+    return [a[0],
+        a[a.length-1]];
+}
