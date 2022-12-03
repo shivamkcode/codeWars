3e3b52c46936ff33d4b0edc5e02ef3367fb3b641
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2022-12-03 06:41:49 +0530

    update Count the divisors of a number.js

--- 7 kyu/Count the divisors of a number.js
+++ 7 kyu/Count the divisors of a number.js
@@ -18,3 +18,10 @@ function getDivisorsCnt(n) {
     }
     return arr.length
 }
+
+function getDivisorsCnt(n) {
+    for (var d = 0, i = n; i > 0; i--) {
+        if (n % i == 0) d++;
+    }
+    return d;
+}
