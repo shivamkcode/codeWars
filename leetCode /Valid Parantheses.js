946b73612e4ca5e7b0262390b4a238faaf587590
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2023-01-02 13:19:16 +0530

    update Valid Parantheses.js

---
+++ leetCode /Valid Parantheses.js
@@ -0,0 +1,22 @@
+var isValid = function(s) {
+    if (s.length % 2 !== 0 ) return false;
+    let str = s.trim().split('');
+    let temp = [];
+    for(let i of str){
+        if(i == '(' || i=='{' || i=='['){
+            temp.push(i);
+        }
+        if(i==')'){
+            if(temp.pop() != '(') return false;
+        }
+        if(i=='}'){
+            if(temp.pop() != '{') return false;
+        }
+        if(i==']'){
+            if(temp.pop() != '[') return false;
+        }
+    };
+    return temp.length > 0 ? false: true;
+};
+
+console.log(isValid([(,[,),]]))
