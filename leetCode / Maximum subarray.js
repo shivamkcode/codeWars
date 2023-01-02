c1b3b26d4f542de49673f8b857845dd61b9f4fcb
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2023-01-02 11:56:07 +0530

    update  Maximum subarray.js

--- leetCode / Maximum subarray.js
+++ leetCode / Maximum subarray.js
@@ -23,7 +23,10 @@ Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.*/
 function maxSub(nums){
     let max = 0
     let sum = nums[1]
-    
+    for(let i=0; i<nums.length; i++){
+        sum += nums[i]
+        if()
+    }
 }
 
 
