037d3d3d29f4fbcf9e8d7930fec9ee7df7902ea5
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2023-01-04 16:51:34 +0530

    update Maximum Swap.js

--- leetCode /Maximum Swap.js
+++ leetCode /Maximum Swap.js
@@ -17,7 +17,7 @@ function maximumSwap(num) {
     let arr = []
     let str = String(num).split('')
     for (let i = 0; i < str.length; i++) {
-        for (let j = 1; j < str.length; j++) {
+        for(let j = 1; j < str.length; j++) {
             if (str[j] > str[i] && j > i) {
                 let s = str,
                 r = s[i]
@@ -27,7 +27,7 @@ function maximumSwap(num) {
             }
         }
     }
-    return arr.length > 1 ? +(arr[0].join('')) : num
+    return arr.length > 1 ? arr : num
 }
 
 console.log(maximumSwap(2736))
