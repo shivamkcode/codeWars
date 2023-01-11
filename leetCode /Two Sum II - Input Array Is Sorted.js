6789ec8417fa2c9d227a3a10c9d8d1142df186b5
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2023-01-11 21:20:17 +0530

    update Two Sum II - Input Array Is Sorted.js

--- leetCode /Two Sum II - Input Array Is Sorted.js
+++ leetCode /Two Sum II - Input Array Is Sorted.js
@@ -28,7 +28,7 @@ Explanation: The sum of -1 and 0 is -1*/
 function twoSums(nums,t){
     for(let i=0; i<nums.length-1;i++){
         for(let j = 1; j<nums.length;j++){
-            if(nums[i] + nums[j] === t){
+            if(i!==j && nums[i] + nums[j] === t){
                 return [i+1,j+1]
             }
         }
