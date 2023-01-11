2e1398a7a8a99e73cf5a025f69e2222abcd6e69e
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2023-01-11 16:12:14 +0530

    update Top K Frequent Elements.js

--- leetCode /Top K Frequent Elements.js
+++ leetCode /Top K Frequent Elements.js
@@ -11,11 +11,11 @@ Example 2:
 Input: nums = [1], k = 1
 Output: [1]*/
 
-function topKFrequent(num,k){
+function topKFrequent(nums,k){
     const counts = {}
     
     for (n of nums) counts[n] = (counts[n] || 0) + 1
     
     //return Object.keys(counts).sort((a, b) => counts[b] - counts[a]).slice(0, k)
     return counts
-}cobs
+}console.log(topKFrequent([1,1,1,2,2,3,4],2))
