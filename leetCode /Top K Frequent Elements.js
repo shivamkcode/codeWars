64595891804422e831004cf4c63b34dd59b43887
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2023-01-11 16:23:08 +0530

    update Top K Frequent Elements.js

--- leetCode /Top K Frequent Elements.js
+++ leetCode /Top K Frequent Elements.js
@@ -16,6 +16,6 @@ function topKFrequent(nums,k){
     
     for (n of nums) counts[n] = (counts[n] || 0) + 1
     
-    //return Object.keys(counts).sort((a, b) => counts[b] - counts[a]).slice(0, k)
-    return counts
+    return Object.keys(counts).sort((a, b) => counts[b] - counts[a]).slice(0, k)
+    //return counts
 }console.log(topKFrequent([1,1,1,2,2,3],2))
